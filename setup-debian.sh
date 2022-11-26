75259935c6bf3c887e7a553e21937fa179309fd3
vukilis <datadrill@datadrill>
2022-11-26 23:33:24 +0100

    script

--- setup-debian.sh
+++ setup-debian.sh
@@ -9,19 +9,23 @@ git
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 
 # configure asdf 
+echo '# asdf #'
 echo '. $HOME/.asdf/asdf.sh' >> ~/.zshrc
 echo 'fpath=(${ASDF_DIR}/completions $fpath)' >> ~/.zshrc
-#echo 'autoload -Uz compinit && compinit' >> ~/.zshrc
-source ~/.zshrc
+echo 'autoload -Uz compinit && compinit' >> ~/.zshrc
+#source ~/.zshrc
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
 # add plugins 
-asdf plugin add python || asdf plugin add nodejs || asdf plugin add terraform || asdf plugin add docker-slim || asdf plugin add docker-compose-v1 
+asdf plugin add python; asdf plugin add nodejs; asdf plugin add terraform; asdf plugin add docker-slim; asdf plugin add docker-compose-v1 
+asdf install python 3.10.8
+asdf global python 3.10.8
 
 # install poetry
 curl -sSL https://install.python-poetry.org | python3 -
 
 # configure poetry
 echo '$HOME/.local/bin' >> ~/.zshrc
-source ~/.zshrc
+echo 'export PATH="/home/linuxmint/.local/bin:$PATH"' >> ~/.zshrc
+#source ~/.zshrc
 poetry config virtualenvs.in-project true
